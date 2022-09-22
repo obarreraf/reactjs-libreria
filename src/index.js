5645538ec24ead716570479b9b7bc3f28901e3b4
Orangel Barrera <obarrerafranco@gmail.com>
2022-09-22 17:24:54 +0200

    New adjustment on MediaIMG && prop-types

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ export { default  as Button } from './Button'
 
 export { default as ContainerFlex } from './ContainerFlex'
 
+export { default as MediaImg } from './MediaImg'
+
 export { default as Text } from './Text'
